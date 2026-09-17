744503368619dd9554ee4cedf56be6f665d028fe
Inzemam <133318740+Inzemam67856@users.noreply.github.com>
2026-09-17 20:34:38 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,17 @@
+/*
+  Creating database and schema for the project 
+
+ABOUT SCRIPT : 
+  AS we dicuseed in our readme that this project is done my Madallion architecture then we have to create the three DB as im using mysql for it 
+
+  */
+
+CREATE DATABASE Datawearhouse;
+USE Datawearhouse;
+CREATE DATABASE bronze;
+USE bronze;
+CREATE DATABASE silver;
+USE silver;
+CREATE DATABASE gold;
+USE gold;
 
